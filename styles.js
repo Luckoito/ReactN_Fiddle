e06cc8753a59b8a43438552df8fb2c1c76c87b44
luckoito <luckoito@icloud.com>
2023-04-17 11:27:41 -0300

    initial commit

---
+++ styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    defaultText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'pink'
+    }
+})
+
+export default styles
